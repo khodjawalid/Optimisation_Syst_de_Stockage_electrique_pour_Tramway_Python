49c295890fd43b759c678ccb6d49110e886963df
KHODJA Walid <khodjawalid2014@gmail.com>
2025-01-06 10:48:05 +0100

    Mise à jour Monte Carlo

--- Functions.py
+++ Functions.py
@@ -4,15 +4,14 @@ import numpy as np
 
 
 # Fonction pour simuler le système et calculer la chute de tension maximale
-def simulation_sys(capacite, seuil_chute, chute, Ptrain, Etrain, temps,Vsst,Req) : 
+def simulation_sys(capacite, seuil_puissance , Ptrain, Etrain, temps, Vsst, Req) : 
     """
     Simule le comportement du système et calcule la chute de tension maximale.
 
     Inputs:
     - capacite (float): Capacité maximale de la batterie (en kWh).
-    - seuil_chute (float): Seuil de chute de tension admissible (en volts).
-    - chute (list[float]): Liste des chutes de tension à chaque instant.
-    - Ptrain (list[float]): Puissance consommée ou produite par le train (en kW).
+    - seuil_puissance (float): Seuil de puissance admissible (W).
+    - Ptrain (list[float]): Puissance consommée ou produite par le train (en W).
     - Etrain (list[float]): Énergie consommée ou produite cumulée par le train (en kWh).
     - temps (list[int]): Liste des indices correspondant aux pas de temps.
     - Vsst (float): Tension de la sous-station (en volts).
@@ -24,7 +23,7 @@ def simulation_sys(capacite, seuil_chute, chute, Ptrain, Etrain, temps,Vsst,Req)
 
     # Initialisation des paramètres
     capa = capacite  # Capacité maximale de la batterie (en énergie)
-    seuil = seuil_chute  # Seuil de chute de tension admissible
+    seuil = seuil_puissance  # Seuil de chute de tension admissible
     rend_batt = 0.9
 
     # Variables initiales
@@ -51,30 +50,41 @@ def simulation_sys(capacite, seuil_chute, chute, Ptrain, Etrain, temps,Vsst,Req)
                 Prhe[t] = Ptrain[t] 
 
             else :   #Remettre la bonne valeur de l'énergie 
-                print("L'énnergie de la batterie a depassé la capacité, remise à niveau")
+                #print("L'énnergie de la batterie a depassé la capacité, remise à niveau")
                 Ebatt = capa
 
         flag1 =True # on met le flag à True afin de détecter le prochaain début d'un cycle de freinage 
 
         if Ptrain[t] > 0 : #Durant l'accélération 
-            if chute[t] > seuil and Ebatt > 0 :
+            if Ptrain[t] > seuil and Ebatt > 0 :
                 if flag2 : #fixer le point du debut d'utilisation de la batterie afin de soustraire l'energie 
                     t1 = t
                     flag2 = False
-                Pbatt[t] = Ptrain[t] #Gestion de la batterie 
+                Pbatt[t] = Ptrain[t]  #Gestion de la batterie 
                 Ebatt -= (Etrain[t+1] - Etrain[t1])*rend_batt #mettre à jour l'energie de la batterie avec rendementt 
 
             flag2 = True
 
-            if chute[t] <= seuil or Ebatt <= 0 : #Si on respecte le seuil ou bien la batterie est déchargée 
+            if Ptrain[t] <= seuil or Ebatt <= 0 : #Si on respecte le seuil ou bien la batterie est déchargée 
                 Plac[t] = Ptrain[t] #On utilise la sst 
 
     
     # Calcul de la tension aux bornes du train avec la batterie
-    Vtrainbatt = 0.5 * Vsst + 0.5 * np.sqrt(Vsst**2 - 4 * Req * Plac)
+    #Vtrainbatt = 0.5 * Vsst + 0.5 * np.sqrt(Vsst**2 - 4 * Req * Plac)
+
+    # Calcul de l'expression sous la racine
+    expression = Vsst**2 - 4 * Req * Plac
+
+    # Initialiser Vtrainbatt avec Vsst 
+    Vtrainbatt = np.ones_like(expression)*Vsst
+
+    # Calculer uniquement pour les indices valides (expression >= 0)
+    valid_indices = expression >= 0
+    Vtrainbatt[valid_indices] = 0.5 * Vsst + 0.5 * np.sqrt(expression[valid_indices])
+
 
     # Calcul de la chute de tension à chaque instant
-    chutebatt = [abs(Vsst - v) for v in Vtrainbatt]
+    chutebatt = np.array([abs(Vsst - v) for v in Vtrainbatt])
 
     # Retourner la chute de tension maximale
     return np.max(chutebatt)
